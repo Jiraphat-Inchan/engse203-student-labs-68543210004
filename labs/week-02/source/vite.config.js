b2ba6406f8b900444f7dd5b4ed23bae071b2103e
Jiraphat Inchan <yuukiigama@gmail.com>
2026-07-24 20:02:08 +0700

    fix(week-02): fix 404 asset path by setting base to ./

--- labs/week-02/source/vite.config.js
+++ labs/week-02/source/vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 
-// TODO: เปลี่ยนให้ตรงชื่อ repository ของตนเอง เช่น engse203-lab02-67123456789
-const repositoryName = "engse203-student-labs-68543210004";
-
 export default defineConfig({
-  base: "./", // <-- แก้บรรทัดนี้จาก `/${repositoryName}/` เป็น "./"
+  base: "./", // <-- ต้องเป็น "./" เท่านั้น ห้ามใส่ชื่อ repository
   build: {
     outDir: "docs",
     emptyOutDir: true,

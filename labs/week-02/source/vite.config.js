97d26456008fca50e6f9bb6c89e45b395db99a16
Jiraphat Inchan <yuukiigama@gmail.com>
2026-07-24 20:24:53 +0700

    fix: update base path in vite.config.js

--- labs/week-02/source/vite.config.js
+++ labs/week-02/source/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // ตั้งค่า base ให้ตรงกับชื่อ repo และ path บน GitHub Pages
   base: '/engse203-student-labs-68543210004/labs/week-02/',
-  // ... การตั้งค่าอื่นๆ
+  // ...
 })

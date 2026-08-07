d3c8f4eeb1ad66bb051ca6ab27dfda8f3ce617ea
Jiraphat Inchan <yuukiigama@gmail.com>
2026-08-07 15:24:32 +0700

    fix(week-04): rebuild assets with relative base path

--- labs/week-04/source/vite.config.js
+++ labs/week-04/source/vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/engse203-student-labs-68543210004-6/labs/week-04/',
+  base: './',
 })
-
-
